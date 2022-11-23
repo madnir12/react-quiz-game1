d3e2d5bd92f816c4f5b95abd2bbf05861fe6a9ed
madnir12 <anfz12001@gmail.com>
2022-11-23 11:31:17 +0500

    Updates

---
+++ precache-manifest.9b5b5dafe82c68144fb3fc7a1216ce06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d13cce493c9ef853d8a18670be0c5a9",
+    "url": "/react-quiz-game1/index.html"
+  },
+  {
+    "revision": "a1ec8bde4e7ba2ce8953",
+    "url": "/react-quiz-game1/static/css/main.f76711c8.chunk.css"
+  },
+  {
+    "revision": "ca334c6fe774937189f1",
+    "url": "/react-quiz-game1/static/js/2.194c40e4.chunk.js"
+  },
+  {
+    "revision": "49b611756b7cdcd9ec9914f14c49889c",
+    "url": "/react-quiz-game1/static/js/2.194c40e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ec8bde4e7ba2ce8953",
+    "url": "/react-quiz-game1/static/js/main.03aa8058.chunk.js"
+  },
+  {
+    "revision": "83318dd08502fcccde54",
+    "url": "/react-quiz-game1/static/js/runtime-main.9f625ed4.js"
+  },
+  {
+    "revision": "093eed4f4091d0792b60b33a1d98448c",
+    "url": "/react-quiz-game1/static/media/Wrong-answer-sound-effect.093eed4f.mp3"
+  },
+  {
+    "revision": "d0c5f2229fb79adc465123e0629687bb",
+    "url": "/react-quiz-game1/static/media/correct-answer.d0c5f222.mp3"
+  }
+]);
